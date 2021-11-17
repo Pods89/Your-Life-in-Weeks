abde82441bbe244bcf770405e2be5c82c701c326
RodrigoVieira11 <93896739+Pods89@users.noreply.github.com>
2021-11-17 21:30:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+a = int(age)
+
+#variaveis
+
+d90 = 90 * 365
+x = d90 - a * 365
+
+weeks_90 = 90 * 52
+y = weeks_90 - a * 52
+
+months_90 = 90 * 12
+z = months_90 - a * 12
+
+
+print(f"You have {x} days, {y} weeks, and {z} months left.")
+
+
+
+
